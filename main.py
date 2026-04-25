418409e6d2b5aef10e89a9e8536ec7e5f88b745d
HassanAbdullahHere <HassanAbdullahHere01@gmail.com>
2026-04-25 11:29:08 +0500

    feat: Basic client brief input and model response configured.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from reqify!")
+from google import genai
+from dotenv import load_dotenv
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+client = genai.Client()
+
+brief = input("Paste your client brief: ")
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents=f"Extract requirements from this client brief: {brief}"
+)
+print(response.text)
